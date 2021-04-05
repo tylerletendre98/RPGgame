bf1f57402d1c9c86658aeb5549325ab5b2494abf
T U <tylerletendre98@gmail.com>
2021-04-05 12:11:23 -0400

    added functions to display winners of fights and new foes that will approach

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+"use strict";
+
+//each of the characters of the story
 let hercules ={
     name : 'Hercules',
     health : 300,
@@ -19,14 +22,21 @@ let hydra = {
     attackPower : 40
 }
 
+//runs the actual game
 function runGame(){
+    displayNewFoe(lion);
     attack(hercules,lion);
-    displayVictor(hercules,lion);
+    displayNewFoe(hydra);
+    attack(hercules,hydra);
+    displayVictor(hercules);
+    endGame();
+    
 }
 
+//runs the actual battle of characters
 function attack(hercules,otherCharacter){
     while (hercules.health >= 0 && otherCharacter.health >= 0){
-        let herculesAttack = prompt(`please choose an attack ${hercules.attacks}: `);
+        let herculesAttack = prompt(`choose an attack for hercules! ${hercules.attacks}: `);
         otherCharacter.health -= hercules.attackPower;
         let otherCharacterAttack =  otherCharacter.attacks[Math.floor(Math.random() * otherCharacter.attacks.length)];
         hercules.health -= otherCharacter.attackPower;
@@ -35,10 +45,29 @@ function attack(hercules,otherCharacter){
 
 }
 
+//displays the victor of the battle
 function displayVictor(hercules,otherCharacter){
+    if (otherCharacter.health >= 0){
     alert(`${hercules.name} has defeated ${otherCharacter.name}`);
+    }
+    else{
+        alert(`${hercules.name} has been defeated!`);
+        endGame();
+    }
 }
 
+//displays when a new foe has shown up
+function displayNewFoe(otherCharacter){
+    alert(`A new foe approaches their name is ${otherCharacter.name} their health is ${otherCharacter.health}.`);
+}
+function endGame(){
+    if(hercules.health < 0){
+    alert('You have defeated the your foes and have captured the guard dog to hell, Congrats you have comepleted the mission!');
+    }
+    else{
+        alert(`${hercules.name} has been defeated! You failed to finish your mission.`);
+    }
+}
 
 
 
